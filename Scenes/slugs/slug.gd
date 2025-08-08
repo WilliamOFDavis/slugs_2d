db595819b7139d1a1b991bb215f68c4c1b6dc49d
William Davis <william.ofdavis@gmail.com>
2025-08-08 15:31:30 +0100

    commit before changes

--- Scenes/slugs/slug.gd
+++ Scenes/slugs/slug.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var jumping = false
 var gravity: float = 50.0
+var is_zooming_out: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play("walk")
@@ -12,17 +13,29 @@ func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
+	$AnimatedSprite2D/RocketLauncher.look_at(get_global_mouse_position())
+	if is_zooming_out:
+		$Camera2D.zoom = lerp($Camera2D.zoom,Vector2(0.4,0.4), 0.2)
+	else:
+		$Camera2D.zoom = lerp($Camera2D.zoom,Vector2(1,1), 0.2)
 	var x_direction: float = Input.get_axis("move_left","move_right")
 	velocity.x = 200 * x_direction
 	if x_direction == -1:
+		$AnimatedSprite2D/RocketLauncher.flip_v = true
 		$AnimatedSprite2D.flip_h = true
 	elif x_direction == 1:
+		$AnimatedSprite2D/RocketLauncher.flip_v= false
 		$AnimatedSprite2D.flip_h = false
 	if !is_on_floor():
 		velocity.y += gravity
 	if is_on_floor() and jumping:
 		jumping = false
 		$AnimatedSprite2D.play("walk")
+	if Input.is_action_just_pressed("right_mouse"):
+		if is_zooming_out:
+			is_zooming_out = false
+		else:
+			is_zooming_out = true
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		$AnimatedSprite2D.play("jump")
 		jumping = true
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Todo
+
+# Slug
+	Add slug state machines for moving and maybe shooting 
+	Add inventory, resource array of weapon resources
+		Can access inventory on side panel
+		Panel persists but swaps inventory for active slug inventory
+	Weapon
+		Texture
+		Projectile type
+		Damage
+		Ammo or uses
+
+# Projectile
+	VelocityComponent
+	DamageComponent
+	ExplosionComponent
+
+# Misc
+	Need some way of generalising explosion
+		Maybe explosion is Area2D, collects all intersecting regions and damages slugs
+		 
